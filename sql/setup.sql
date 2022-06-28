23b4b2a30fb4325286b425e73b79cfa4ec6f4dd4
Mary Martinez <mary.diana.martinez@gmail.com>
2022-06-28 15:49:49 -0700

    setup sql is ready

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,20 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS userstodos;
+DROP TABLE IF EXISTS todos;
+
+CREATE TABLE userstodos (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email VARCHAR NOT NULL UNIQUE,
+  password_hash VARCHAR NOT NULL
+);
+
+CREATE TABLE todos (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT,
+  description VARCHAR NOT NULL,
+  complete BOOLEAN NOT NULL DEFAULT(false),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES userstodos(id)
+);
