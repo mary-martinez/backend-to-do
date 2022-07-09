868974e0628378a750968ac975821872d5e0b1f7
Mary Martinez <mary.diana.martinez@gmail.com>
2022-07-09 11:54:56 -0700

    users/sessions route is working and passing the test

--- __tests__/users.tests.js
+++ __tests__/users.tests.js
@@ -2,12 +2,22 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { UserService } = require('../lib/services/UserService');
 // const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   email: 'test@example.com',
   password: '123456',
 };
+
+// const signUpAndIn = async (userProps = {}) => {
+//   const password = userProps.password ?? mockUser.password;
+//   const agent = request.agent(app);
+//   const user = await UserService.create({ ...mockUser, ...userProps });
+//   const { email } = user;
+//   await agent.post('/api/v1/users/sessions').send({ email, password });
+//   return [agent, user];
+// };
 
 describe('users', () => {
   beforeEach(() => {
@@ -21,6 +31,14 @@ describe('users', () => {
       id: expect.any(String),
       email
     });
+  });
+
+  it('signs in a user and creates a cookie', async () => {
+    const res1 = await request(app).post('/api/v1/users').send(mockUser);
+    console.log('res1', res1.body);
+    const res = await request(app).post('/api/v1/users/sessions').send(mockUser);
+    console.log('res.body', res.body);
+    expect(res.status).toEqual(200);
   });
 
   afterAll(() => {
