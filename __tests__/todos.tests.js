3a8632979d85e16e241cc6ecc260ea0d65eba212
Mary Martinez <mary.diana.martinez@gmail.com>
2022-07-10 17:10:07 -0700

    GET todos is working

--- __tests__/todos.tests.js
+++ __tests__/todos.tests.js
@@ -3,10 +3,16 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const { UserService } = require('../lib/services/UserService');
+const { Todo } = require('../lib/models/Todo');
 
 const mockUser = {
   email: 'test@example.com',
   password: '123456',
+};
+
+const mockUser2 = {
+  email: 'unrea@aol.com',
+  password: 'apples'
 };
 
 const signUpAndIn = async (userProps = {}) => {
@@ -37,6 +43,27 @@ describe('todos', () => {
       complete: false,
       createdAt: expect.any(String),
     });
+  });
+  it('GET /api/v1/todos should return a list of todos for the authenticated user', async () => {
+    const [agent, user] = await signUpAndIn();
+    const user2 = await UserService.create(mockUser2);
+    const userTodo = await Todo.insert({
+      description: 'Take a nap',
+      userId: user.id
+    });
+    await Todo.insert({
+      description: 'Get groceries',
+      userId: user2.id
+    });
+    const res = await agent.get('/api/v1/todos');
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual({
+      ...userTodo,
+      id: expect.any(String),
+      complete: false,
+      createdAt: expect.any(String)
+    });
+
   });
   afterAll(() => {
     pool.end();
